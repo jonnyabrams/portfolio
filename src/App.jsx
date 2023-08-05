abc7dbce131553e4a5bb3eaeebc8dffbff1b816f
jonnyabrams <jonnyabrams@hotmail.com>
2023-08-05 21:45:57 +0100

    App.jsx imports/structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,37 @@
+import { BrowserRouter as Router } from "react-router-dom";
+
+import {
+  About,
+  Contact,
+  Experience,
+  Feedback,
+  Hero,
+  Navbar,
+  Tech,
+  Works,
+  StarsCanvas,
+} from "./components";
+
 const App = () => {
-  return <div>Jonny Abrams portfolio</div>;
+  return (
+    <Router>
+      <div className="relative z-0 bg-primary">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+          <Navbar />
+          <Hero />
+        </div>
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
+        <Feedback />
+        <div className="relative z-0">
+          <Contact />
+          <StarsCanvas />
+        </div>
+      </div>
+    </Router>
+  );
 };
 
 export default App;
