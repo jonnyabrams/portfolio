c6b82af6791fed00075b0a595fa65b467e7a4de9
jonnyabrams <jonnyabrams@hotmail.com>
2023-08-06 09:26:49 +0100

    jumping circle to click to next section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -20,11 +20,26 @@ const Hero = () => {
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
             I specialise in TypeScript, React, React Native, Next.js, Node, Ruby
-            on Rails and responsive design. Go on, give the computer a spin...
+            on Rails and responsive design.
           </p>
         </div>
       </div>
       <ComputerCanvas />
+      <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center">
+        <a href="#about">
+          <div className="w[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
+            <motion.dev
+              animate={{ y: [0, 24, 0] }}
+              transition={{
+                duration: 1.5,
+                repeat: Infinity,
+                repeatType: "loop",
+              }}
+              className="w-3 h-3 rounded-full bg-secondary mb-1"
+            />
+          </div>
+        </a>
+      </div>
     </section>
   );
 };
