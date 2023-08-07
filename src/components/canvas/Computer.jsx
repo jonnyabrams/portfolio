89c0d4c6ad3a44e2610481a262c82a763df36c3a
jonnyabrams <jonnyabrams@hotmail.com>
2023-08-07 08:20:17 +0100

    feat: technology balls

--- src/components/canvas/Computer.jsx
+++ src/components/canvas/Computer.jsx
@@ -3,8 +3,9 @@ import { Suspense, useEffect, useState } from "react";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 
-import CanvasLoader from "../Loader";
+import Loader from "../Loader";
 
+// eslint-disable-next-line react/prop-types
 const Computer = ({ isMobile }) => {
   const computer = useGLTF("./desktop_pc/scene.gltf");
   return (
@@ -59,7 +60,7 @@ const ComputerCanvas = () => {
       camera={{ position: [20, 3, 5], fov: 25 }}
       gl={{ preserveDrawingBuffer: true }}
     >
-      <Suspense fallback={<CanvasLoader />}>
+      <Suspense fallback={<Loader />}>
         <OrbitControls
           enableZoom={false}
           maxPolarAngle={Math.PI / 2}
